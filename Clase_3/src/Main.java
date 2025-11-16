cab24e1d9043f27367b418d2474b169a09e780d0
FernandoMoyano2121 <fernandomoyano21@hotmail.com>
2025-11-16 11:01:20 -0300

    clase_3

---
+++ Clase_3/src/Main.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        String nombre = "Ana";
+//        int edad = 25;
+//        double altura = 1.68;
+//        boolean estudiante= true;
+//
+//        System.out.println(nombre);
+//        System.out.println(edad);
+//        System.out.println(altura);
+//        System.out.println(estudiante);
+
+
+//        HashSet<String> nombres = new HashSet<>();
+//        nombres.add("Ana");
+//        nombres.add("Ana");
+//
+//        System.out.println(nombres);
+
+
+//        HashMap<String, Integer> edades = new HashMap<>();
+//
+//        edades.put("Juan", 25);
+//        edades.put("Ana", 30);
+//
+//        System.out.println(edades);
+
+
+//        int[] temperaturas ={20, 25, 30};
+//
+//        ArrayList<Integer> notas = new ArrayList<>();
+//        notas.add(7);
+//        notas.add(10);
+//        notas.add(3);
+//
+//        System.out.println("temperaturas[0] = " + temperaturas[0]);
+//        System.out.println("Notas del alumno =" + notas);
+
+//        Aritméticos: +, -, *, /, %
+//        Relacionales: >, <, >=, <=, ==, !=
+//        Condicionales: &&, ||
+//        Todos devuelven valores booleanos
+
+//        System.out.println("Inicio");
+//        System.out.println("Desarrollo");
+//        System.out.println("Fin");
+
+//       if/si(condicion) {
+//          ...código a ejecutar
+//       }else(de lo contraio){
+//          ...código a ejecutar
+//    }
+//
+
+//     int edad = 16;
+//
+//     if(edad >= 18){
+//         System.out.println("Eres Mayor de edad");
+//     }else{
+//         System.out.println("Eres menor de edad");
+//     }
+
+
+//        int dia = 5;
+//
+//        switch(dia){
+//            case 1:
+//                System.out.println("Lunes");break;
+//            case 2:
+//                System.out.println("Martes");break;
+//            default:
+//                System.out.println("Otro dia");
+//        }
+
+//        int i = 1;
+//        while(i <= 5){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 9;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while(i <=5);
+
+
+//   for(inicialización; condición; actualización){
+//        ...código a ejecutar
+//    }
+
+//       for(int j = 1; j <= 5; j++){
+//           System.out.println(j);
+//       }
+
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Ingresa tu nombre: ");
+//        String nombre = sc.nextLine();
+//
+//        System.out.println("Ingresa tu edad: ");
+//        int edad = sc.nextInt();
+//
+//        System.out.println("Tu nombre es : " + nombre + " tienes : " + edad + " años");
+//        sc.close();
+
+    }
+}
