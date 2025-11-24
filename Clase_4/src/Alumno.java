a6fa78e06c22bc0c53dfbacc4e8840287e771b59
FernandoMoyano2121 <fernandomoyano21@hotmail.com>
2025-11-24 19:09:44 -0300

    Clase_4

---
+++ Clase_4/src/Alumno.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Alumno extends Persona {
+    private String matricula;
+    private Integer nota;
+    private ArrayList<String> materias;
+
+
+    public Alumno(String matricula, String nombre, String apellido){
+        super(nombre, apellido);
+        this.matricula = matricula;
+        this.nota = 0;
+        this.materias = new ArrayList<>();
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public ArrayList<String> getMaterias(){
+        return materias;
+    }
+
+    public void setMaterias(ArrayList<String> materias){
+        this.materias=materias;
+    }
+
+    public void agregarMateria(String materia){
+        this.materias.add(materia);
+    }
+}
